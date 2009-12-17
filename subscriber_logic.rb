355a05e0e771c3a57317097f8c404c8ebe0fc1eb
Nic Benders <nic.benders@vocel.com>
2009-12-17 08:41:14 -0800

    Some real subscriber delete rules

--- subscriber_logic.rb
+++ subscriber_logic.rb
@@ -14,11 +14,45 @@ class SubscriberLogic
   end
 end
 
-module Alltelportal
+# alltelportal_prod.subscribers#phonenumber
+module AlltelportalProd
   module Subscribers
     class Phonenumber < SubscriberLogic
       def delete_subscriber( phonenumber )
         "-- Alltel subscriber should not be DB deleted"
+      end
+    end
+  end
+end
+
+# alltelvmg_prod.subscribers#phonenumber
+module AlltelvmgProd
+  module Subscribers
+    class Phonenumber < SubscriberLogic
+      def delete_subscriber( phonenumber )
+        "-- Alltel VMG subscriber should not be DB deleted"
+      end
+    end
+  end
+end
+
+# pillphone.user#number
+module Pillphone
+  module User
+    class Number < SubscriberLogic
+      def delete_subscriber( phonenumber )
+        "-- Pillphone subscriber should not be DB deleted"
+      end
+    end
+  end
+end
+
+# pillphone.settings#sms
+module Pillphone
+  module Settings
+    class Sms < SubscriberLogic
+      def delete_subscriber( phonenumber )
+        "-- Pillphone subscriber should not be DB deleted"
       end
     end
   end
