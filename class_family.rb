719af50a88dfcaf1cb0f0c868f2795fc9899175b
Nic Benders <nic.benders@gmail.com>
2009-12-17 00:05:56 -0800

    Make number of arguments to ClassFamily flexible

--- class_family.rb
+++ class_family.rb
@@ -1,22 +1,23 @@
 module ClassFamily
-  def new( schema = nil, table = nil, column = nil)
-    new_logic_class( schema, table, column ) || super()
+  def new( *args )
+    new_logic_class( *args ) || super()
   end
 
-  def new_logic_class( schema, table, column )
-    [schema, table, column].each do |arg|
+  def new_logic_class( *args )
+    return false if args.empty?
+    args.each do |arg|
       return false if arg.nil?
     end
 
-    class_name = self.class_name( schema, table, column )
+    class_name = self.class_name( *args )
     klass = self.find_constant( class_name )
     if klass && klass.respond_to?('new_logic_class')
       klass.new
     end
   end
 
-  def class_name( schema, table, column )
-    name = "#{schema.capitalize}::#{table.capitalize}::#{column.capitalize}"
+  def class_name( *args )
+    name = args.map{|x| x.capitalize}.join('::')
     name.gsub(/_(\w)/) { $1.capitalize }
   end
 
