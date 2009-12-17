0723051f9729187d2b46501202ed693da226a6f6
Nic Benders <nic.benders@gmail.com>
2009-12-16 21:31:44 -0800

    arg0, exit codes, etc

--- find-subscriber.rb
+++ find-subscriber.rb
@@ -9,25 +9,36 @@ PASSWORD= "E7RSG9eYTwMpDsLS"
 
 $db = Mysql.new(HOSTNAME, USERNAME, PASSWORD)
 
-search_number = "2495155889"
+search_number = ARGV[0]
 
 products = []
-File.open("test-list.txt") do |file|
+File.open("table-list.txt") do |file|
   file.each do |line|
     table_name, field_name = line.split('#')
     product = table_name.split(/\W+/).first
 
-    st = $db.prepare("SELECT * FROM #{table_name} WHERE CAST(#{field_name} AS CHAR) LIKE ?;")
-    st.execute("%#{search_number}")
+    begin
+      st = $db.prepare("SELECT * FROM #{table_name} WHERE CAST(#{field_name} AS CHAR) LIKE ?;")
+      st.execute("%#{search_number}")
+    rescue Mysql::Error => e
+      STDERR.puts "ERROR on #{line.chomp}: #{e.to_s}"
+      next
+    end
 
     if st.num_rows > 0
       products << product unless products.include?(product)
-      puts "FOUND in #{table_name}#{field_name}"
+      STDERR.puts "FOUND in #{table_name}#{field_name}"
     end
   end
 end
 
 puts
-puts "Subscriber found in #{products.join(', ')} products" if products.any?
+if products.any?
+  puts "Subscriber #{search_number} found in #{products.join(', ')} products"
+  exit(0)
+else
+  puts "Subscriber #{search_number} NOT FOUND in any products"
+  exit(1)
+end
 
 $db.close
